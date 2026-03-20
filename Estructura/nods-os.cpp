4c60154f337c1bd5b43cac5aceb3708826f1f1d8
Carlos Marin Cardozo <carlosmarcar05@gmail.com>
2026-03-20 19:21:30 -0400

    Update nods-os.cpp

--- Estructura/nods-os.cpp
+++ Estructura/nods-os.cpp
@@ -2,26 +2,38 @@
 using namespace std;
 
 struct Nodo {
-    string apellido;
-    Nodo* siguiente;
+    string nombre;
+    Nodo* siguiente; 
 };
+
 int main() {
+   
+    Nodo* head = new Nodo{"Carlos", nullptr};
+    Nodo* nodo2 = new Nodo{"Edson", nullptr};
+    Nodo* nodo3 = new Nodo{"Ernesto", nullptr};
+    Nodo* nodo4 = new Nodo{"Ian", nullptr};
+    Nodo* nodo5 = new Nodo{"Luis", nullptr};
+
     
-    Nodo* nodo1 = new Nodo();
-    Nodo* nodo2 = new Nodo();
+    head->siguiente = nodo2;
+    nodo2->siguiente = nodo3;
+    nodo3->siguiente = nodo4;
+    nodo4->siguiente = nodo5;
+    nodo5->siguiente = nullptr; 
 
-    nodo1->apellido = 'marin';
-    nodo1->siguiente = nodo2;
-    nodo2->apellido = 'lopez';
-    nodo2->siguiente = nullptr; 
+    // Recorrer y mostrar todos los valores
+    Nodo* actual = head;
+    while (actual != nullptr) {
+        cout << actual->nombre << endl;
+        actual = actual->siguiente;
+    }
 
-    cout << "Nodo 1: " << nodo1->apellido << endl;
-    cout << "Nodo 2: " << nodo1->siguiente->apellido << endl;
-    
-    while
-
-  
-    delete nodo1;
+    // Liberar memoria
+    delete head;
     delete nodo2;
+    delete nodo3;
+    delete nodo4;
+    delete nodo5;
+
     return 0;
 }
